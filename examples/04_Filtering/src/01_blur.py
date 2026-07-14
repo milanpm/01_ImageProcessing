4645647f34326a78e2db4d66a8e55beca8ffcbb1
Alex <xenrl@naver.com>
2026-07-14 12:35:04 +0900

    Fix avarage blur example

--- examples/04_Filtering/src/01_blur.py
+++ examples/04_Filtering/src/01_blur.py
@@ -1,25 +1,19 @@
-from pathlib import Path
 import cv2
+from pathlib import Path
 
 ROOT = Path(__file__).resolve().parents[3]
-image_path = "images/sample.png"
+image_path = ROOT / "images" / "sample.png"
 
-img = cv2.imread(image_path)
+image = cv2.imread(str(image_path))
 
-if img is None:
-    print("Error: image file not found.")
-    exit()
+if image is None:
+    print("Error: Image file not found.")
+    raise SystemExit
 
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+average_blur = cv2.blur(image, (5, 5))
 
-blur = cv2.GaussianBlur(gray, (5, 5), 0)
-
-_, thresh = cv2.threshold(blur, 127, 255, cv2.THRESH_BINARY)
-
-cv2.imshow("Original", img)
-cv2.imshow("Gray", gray)
-cv2.imshow("Blur", blur)
-cv2.imshow("Threshold", thresh)
+cv2.imshow("Original", image)
+cv2.imshow("Average Blur", average_blur)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
